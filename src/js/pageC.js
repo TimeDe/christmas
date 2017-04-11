a8acf64dde568c04e1e8213fcd7a7106d06d7c62
TimeDe <18231021746@163.com>
2017-04-11 13:46:25 +0800

    update
    Extract the common style, the design page C close window effect

--- src/js/pageC.js
+++ src/js/pageC.js
@@ -1,3 +1,41 @@
 function pageC(element){
-    alert('pageC');
+    this.$window = element.find('.window');
+    this.$leftWin = this.$window.find('.window-left');
+    this.$rightWin = this.$window.find('.window-right');
+    this.$sceneBg = this.$window.find('.window-scene-bg');
+    this.$closeBg = this.$window.find('.window-close-bg');
+
+    this.$sceneBg.transition({
+        'opacity' : 0
+    }, 3000);
+    this.$closeBg.css('transform', 'translateZ(0deg)');
+    this.$closeBg.transition({
+        'opacity' : 1
+    }, 5000);
+
+    this.closeWindow();
 }
+
+pageC.prototype.closeWindow = function () {
+    var count = 1;
+    var complete = function () {
+        ++count;
+        if(count == 2){
+            alert('窗关闭')
+        }
+    };
+
+    var bind = function (element) {
+        element.addClass('close').one('animationend webkitAnimationEnd', function () {
+            complete();
+        });
+    };
+
+    bind(this.$leftWin);
+    bind(this.$rightWin);
+}
+
+
+
+
+
